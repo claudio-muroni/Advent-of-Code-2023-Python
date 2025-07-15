f604253940f6733810a9317836991ee6f8219b7c
Claudio Muroni <muroni.claudio@gmail.com>
2025-07-15 23:16:40 +0200

    Day 01 part 2 solved

--- Day01.py
+++ Day01.py
@@ -1,35 +1,46 @@
 tot = 0
+tot2 = 0
+num_list = [("one","1"), ("two","2"), ("three","3"), ("four","4"), ("five","5"), ("six","6"), ("seven","7"), ("eight","8"), ("nine","9")]
 
 with open('Input_01_1.txt', 'r') as file:
 
     for line in file:
 
-        # WRONG: there are case in which letters are shared but you have to take the first usage
-        line = line.replace("one","1")
-        line = line.replace("two","2")
-        line = line.replace("three","3")
-        line = line.replace("four","4")
-        line = line.replace("five","5")
-        line = line.replace("six","6")
-        line = line.replace("seven","7")
-        line = line.replace("eight","8")
-        line = line.replace("nine","9")
-
         first_digit = ""
         last_digit = ""
+        first_index = -1
+        last_index = -1
 
-        for c in line:
-            if c.isdigit():
+        for i in range(len(line)):
+            if line[i].isdigit():
                 if first_digit == "":
-                    first_digit = c
+                    first_digit = line[i]
+                    first_index = i
                 else:
-                    last_digit = c
+                    last_digit = line[i]
+                    last_index = i
         
         if last_digit == "":
             last_digit = first_digit
+            last_index = first_index
 
         num = first_digit + last_digit
-        print(num)
         tot = tot + int(num)
 
-print(tot)   
+        for (x,y) in num_list:
+            i_str = line.find(x)
+            if i_str > -1 and i_str < first_index:
+                first_digit = y
+                first_index = i_str
+        
+        for (x,y) in num_list:
+            i_str = line.rfind(x)
+            if i_str > -1 and i_str > last_index:
+                last_digit = y
+                last_index = i_str
+
+        num2 = first_digit + last_digit
+        tot2 = tot2 + int(num2)
+
+print(tot)
+print(tot2) 
