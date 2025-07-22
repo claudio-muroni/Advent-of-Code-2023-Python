720cd2d45690397b5be7bb5baeac9c1504c41701
Claudio Muroni <muroni.claudio@gmail.com>
2025-07-22 23:14:42 +0200

    Day 02 part 2 solved

--- Day02_2.py
+++ Day02_2.py
@@ -17,8 +17,15 @@ with open('Input_02_1.txt','r') as file:
                 cube_num = cube.split()[0]
                 cube_color = cube.split()[1]
 
+                match cube_color:
+                    case "red": 
+                        if int(cube_num) > min_red: min_red = int(cube_num)
+                    case "green": 
+                        if int(cube_num) > min_green: min_green = int(cube_num)
+                    case "blue": 
+                        if int(cube_num) > min_blue: min_blue = int(cube_num)
+                        
+        tot = tot + (min_red*min_green*min_blue)
                 
-                
-       
 
 print(tot)
