821f7f8327d531aa24acf58456869f28deab3c9a
Claudio Muroni <muroni.claudio@gmail.com>
2025-08-14 10:59:29 +0200

    Day 05 part 1 wip

---
+++ Day05_1.py
@@ -0,0 +1,38 @@
+
+n_line = 1
+seed_map = {}
+
+with open('Input_05_1.txt','r') as file:
+    for line in file:
+        if n_line == 1:
+            seeds =  list(map(int, line[line.find(':')+1:].split()))
+            print(seeds)
+            
+        
+        if line[0].isdigit():
+            parameters = list(map(int, line.split()))
+            for i in range(0,parameters[2]):
+                seed_map[parameters[1]+i]=parameters[0]+i
+
+        if not line[0].isdigit() and len(seed_map) > 0:  
+            
+            for pos in range(0,len(seeds)):
+                if seeds[pos] in seed_map:
+                    seeds[pos] = seed_map[seeds[pos]]
+
+            #print(seed_map)
+            seed_map = {}
+            print(seeds)
+        
+        n_line += 1
+
+
+for pos in range(0,len(seeds)):
+    if seeds[pos] in seed_map:
+        seeds[pos] = seed_map[seeds[pos]]
+
+#print(seed_map)
+seed_map = {}
+print(seeds)
+
+print(min(seeds))
