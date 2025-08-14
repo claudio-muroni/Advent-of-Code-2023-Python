2416c4d7398abc46ba81a1bb0c08dbf63c241820
Claudio Muroni <muroni.claudio@gmail.com>
2025-08-14 11:30:09 +0200

    Day 05 part 1 wip

--- Day05_1.py
+++ Day05_1.py
@@ -1,39 +1,39 @@
-# it is not sustainable to save each mapping combination in a dict
+# removed usage of dictionaries
+# still inefficent, need to avoid useless attempts
 
 n_line = 1
-seed_map = {}
 
 with open('Input_05_1.txt','r') as file:
     for line in file:
         if n_line == 1:
             seeds =  list(map(int, line[line.find(':')+1:].split()))
+            new_seeds = []
             print(seeds)
             
         
         if line[0].isdigit():
             parameters = list(map(int, line.split()))
             for i in range(0,parameters[2]):
-                seed_map[parameters[1]+i]=parameters[0]+i
 
-        if not line[0].isdigit() and len(seed_map) > 0:  
+                if len(seeds) == 0: break
+
+                if parameters[1]+i in seeds:
+                    seeds.remove(parameters[1]+i)
+                    new_seeds.append(parameters[0]+i)
+
+
+        if not line[0].isdigit() and len(new_seeds) > 0:  
             
-            for pos in range(0,len(seeds)):
-                if seeds[pos] in seed_map:
-                    seeds[pos] = seed_map[seeds[pos]]
+            seeds.extend(new_seeds)
+            new_seeds = []
 
-            #print(seed_map)
-            seed_map = {}
-            print(seeds)
+            #print(seeds)
         
         n_line += 1
 
+seeds.extend(new_seeds)
+new_seeds = []
 
-for pos in range(0,len(seeds)):
-    if seeds[pos] in seed_map:
-        seeds[pos] = seed_map[seeds[pos]]
-
-#print(seed_map)
-seed_map = {}
-print(seeds)
+#print(seeds)
 
 print(min(seeds))
